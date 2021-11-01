ed8be9b0a60ec397658c5c2196cecc02b9ed95d3
declaresownproperty <simonlin.rqmmw@slmail.me>
2021-11-01 18:59:38 +1100

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,13 @@ pub fn add(a: i32, b: i32) -> i32 {
     return a + b;
 }
 
-#[test]
-fn add_test() {
-    let a = 2;
-    let b = 3;
-    assert_eq!(add(a, b), 5);
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn add_test() {
+        let a = 2;
+        let b = 3;
+        assert_eq!(add(a, b), 5);
+    }
 }
